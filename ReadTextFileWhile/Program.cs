e8b903c40c91bde4f53cea0f7c65e79ea62dc198
jsales3 <jovonsales@gmail.com>
2018-03-03 14:56:09 -0500

    first commit

---
+++ ReadTextFileWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTextFileWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader myReader = new StreamReader("Values.txt");
+            string line = "";
+
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+
+         
+            }
+
+            Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$");
+            Console.WriteLine(" ");
+            Console.WriteLine("$                    $");
+            Console.WriteLine(" ");
+            Console.WriteLine("$  jsales3@kent.edu  $");
+            Console.WriteLine(" ");
+            Console.WriteLine("$                    $");
+            Console.WriteLine(" ");
+            Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$");
+            Console.WriteLine(" ");
+            Console.WriteLine("OK, we are done!");
+
+            myReader.Close();
+            Console.ReadLine();
+        }
+    }
+}
